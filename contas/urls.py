9dda80bb23e31e720b0e2516558e36998ecb8798
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-31 21:07:45 -0300

    CRUD - Read

--- contas/urls.py
+++ contas/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.listagem),
     path('home/', views.home),
 ]
--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,18 @@
+import datetime
+
 from django.shortcuts import render
+
+from .models import Transacao
 
 
 def home(request):
-    return render(request, 'contas/html/home.html')
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
+    return render(request, 'contas/html/home.html', data)
+
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/html/listagem.html', data)
